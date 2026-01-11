e72fa6eb13a4651828e03f977e341f1f51dd35d8
Yasin Quluzada <yasinquluzad850@gmail.com>
2026-01-11 23:22:44 +0400

    Refactor histogram plotting for student grades
    Refactored frequency function to directly plot histogram with specified bins and limits.

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-"""4-frequency.py - Histogram of student grades for Project A."""
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
 
-def frequency():
-    """Plot a histogram of student grades with 10-unit bins and black outlines."""
-    np.random.seed(5)
-    student_grades = np.random.normal(68, 15, 50)
-    plt.figure(figsize=(6.4, 4.8))
-    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor="black")
-    plt.xlabel("Grades")
-    plt.ylabel("Number of Students")
-    plt.title("Project A")
-    plt.show()
+plt.hist(student_grades,
+         bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+         edgecolor='black')
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title("Project A")
+plt.xlim((0, 100))
+plt.xticks(np.arange(0, 101, 10))
+plt.ylim((0, 30))
+plt.show()
