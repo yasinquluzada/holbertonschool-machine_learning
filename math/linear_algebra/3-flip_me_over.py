df2615d8e230486917acf8a95d5f9bce4bd56daf
yasinquluzada <yasinquluzad850@gmail.com>
2026-02-01 16:33:05 +0400

    3-flip_me_over.py

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def matrix_transpose(matrix):
-    return [list(col) for col in zip(*matrix)]
+    #return [list(col) for col in zip(*matrix)]
+
+
+
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix(i))):
+           matrix[i][j] = matrix[j][i]
