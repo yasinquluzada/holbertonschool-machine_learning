9028703477182e1603f582d2cd2b460d04b66de6
yasinquluzada <yasinquluzad850@gmail.com>
2026-02-02 09:21:40 +0000

    Add np_matmul using NumPy matmul operator

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""NumPy matrix multiplication."""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Perform matrix multiplication using NumPy without loops or conditionals."""
+    return mat1 @ mat2
