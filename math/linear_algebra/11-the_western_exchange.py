313127e4a0cb4445611a20200cdd5426867939f0
yasinquluzada <yasinquluzad850@gmail.com>
2026-02-02 09:17:17 +0000

    Add np_transpose

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Numpy transpose utility."""
+
+
+def np_transpose(matrix):
+    """Return the transpose of a matrix as a new numpy.ndarray."""
+    return matrix.transpose()
