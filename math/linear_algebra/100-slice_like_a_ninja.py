6d650631561563550fff9fd5709050c2c78a0849
Yasin Quluzada <yasinquluzad850@gmail.com>
2026-02-02 05:51:41 +0400

    Update 100-slice_like_a_ninja.py

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Slice a NumPy ndarray along specified axes."""
+
 
 
 def np_slice(matrix, axes={}):
-    """Return a sliced copy of a NumPy ndarray using an axes spec."""
+    
     slices = [slice(None)] * matrix.ndim
     for axis, slc in axes.items():
         slices[axis] = slice(*slc)
