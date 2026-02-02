fb628fc368c3ee398eba873744e8219ed02c70ef
yasinquluzada <yasinquluzad850@gmail.com>
2026-02-02 09:15:29 +0000

    Add np_shape function

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Return the shape of a numpy.ndarray."""
+
+
+def np_shape(matrix):
+    """Return the shape of a numpy.ndarray as a tuple of integers."""
+    return matrix.shape
