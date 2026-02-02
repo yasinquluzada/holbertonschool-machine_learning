633ea5268a773c210dca80aaaa6fb96dadec4b27
yasinquluzada <yasinquluzad850@gmail.com>
2026-02-02 09:04:16 +0000

    Add cat_arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Array concatenation utility."""
+
+
+def cat_arrays(arr1, arr2):
+    """Return a new list containing arr1 followed by arr2."""
+    return list(arr1) + list(arr2)
